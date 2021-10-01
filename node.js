2464ad9164af85fde90dd6c3738f14e66d685be8
Tartarus <Empty>
2021-10-01 21:52:08 +0300

    EJS Remake

--- node.js
+++ node.js
@@ -1,20 +1,25 @@
-const express = require('express')
-const server = express()
+const http = require('http')
 const path = require('path')
+const express = require('express')
+const mysql = require('mysql')
+
+const server = express()
 const port = 80
 
+// Embedded JavaScript Innit
+server.set('views', path.join(__dirname, '/front-end/ejs'));
+server.set('view engine', 'ejs')
+
 server.get('/', (req, res) => {
-    console.log("/")
-    res.sendFile(path.join(__dirname, '/front-end/html/index.html'))
+    res.render('index', {
+        title: "My Title"
+    });
 })
-server.get('/assets/Logo_1.png', (req, res) => {
-    res.sendFile(path.join(__dirname, '/front-end/assets/Logo_1.png'))
+server.get('/*(.png|.jpg)', (req, res) => {
+    res.sendFile(path.join(__dirname, `/front-end/assets${req.url}`))
 })
-server.get('/assets/burger.jpg', (req, res) => {
-    res.sendFile(path.join(__dirname, '/front-end/assets/burger.png'))
-})
-server.get('/css/style.css', (req, res) => {
-    res.sendFile(path.join(__dirname, '/front-end/css/style.css'))
+server.get('/*.css', (req, res) => {
+    res.sendFile(path.join(__dirname, `/front-end/css${req.url}`))
 })
 server.listen(port, () => {
     console.log(`Server is Running on port ${port}`)
