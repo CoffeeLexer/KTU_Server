bd566ff6c7665ff34aa81f02a73a5731c30a5e0d
Tartarus <Empty>
2021-10-04 23:30:10 +0300

    EJS and ERROR handling update

--- node.js
+++ node.js
@@ -3,8 +3,23 @@ const path = require('path')
 const express = require('express')
 const mysql = require('mysql')
 
+const script = require('./script')
+const assert = require("assert");
+
 const server = express()
 const port = 80
+
+const database = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'system'
+})
+
+database.connect((err) =>{
+    if(err) throw err;
+    console.log(`Database Connected`)
+})
 
 // Embedded JavaScript Innit
 server.set('views', path.join(__dirname, '/front-end/ejs'));
@@ -15,11 +30,18 @@ server.get('/', (req, res) => {
         title: "My Title"
     });
 })
+// 404
 server.get('/*(.png|.jpg)', (req, res) => {
     res.sendFile(path.join(__dirname, `/front-end/assets${req.url}`))
 })
 server.get('/*.css', (req, res) => {
     res.sendFile(path.join(__dirname, `/front-end/css${req.url}`))
+})
+server.use((req, res, next) => {
+    res.status(404).render('index_error', {
+        title: "ERROR 404",
+        error: `ERROR 404 Link '${req.url.substr(1)}' is not found`
+    })
 })
 server.listen(port, () => {
     console.log(`Server is Running on port ${port}`)
